858838fee281bdd97545afb132fb68ba1af3ec99
unknown <mpg@HIGHQ-L025.highq-it.de>
2016-11-08 16:20:38 +0100

    Initial Commit

---
+++ data.js
@@ -0,0 +1,7 @@
+var data = 
+{
+'numbers' : {
+	'de' : ["eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn"],
+	'mg' : ["iray", "rua", "telo", "efatra", "dimy", "enina", "fito", "valo", "sivy", "folo"]
+}
+}
---
+++ miteny.js
@@ -0,0 +1,46 @@
+﻿var app = angular.module('mitenyMain', ['mitenyUtil']);
+app.controller('Main', ['checkAnswer', '$scope', function Main(checkAnswer, $scope) {
+	this.getQuestion = function getQuestion() {
+		return data['numbers']['de'][index]
+	}
+	this.check = function check() {
+		console.log($scope)
+		return checkAnswer.checkAnswerF($scope.answer, $scope)
+  };
+  
+	newRandomQuestion($scope);
+}]);
+angular.module('mitenyUtil', [])
+.factory('checkAnswer', function() {
+
+	
+	var checkAnswerF = function checkAnswerF(guess, $scope) {
+
+		if (guess.toUpperCase() == getCorrectAnswer().toUpperCase()) {
+			correctGuess($scope)
+			return "Richtig!"
+		} else {
+			return "Falsch";
+		}
+	}
+	return {
+		checkAnswerF : checkAnswerF
+	};
+	
+});
+
+
+
+
+function newRandomQuestion($scope) {
+	index = parseInt(Math.random()*10, 10)
+	$scope.answer = ""
+}
+function getCorrectAnswer() {
+	return data['numbers']['mg'][index]
+}
+
+function correctGuess($scope) {
+	newRandomQuestion($scope);
+	
+}
