eebb98c3ef5442abce56044928f9d3f78868104c
panschk <panschk@googlemail.com>
2016-11-11 11:22:28 +0100

    memory game

--- miteny.js
+++ miteny.js
@@ -20,6 +20,11 @@ app.controller('Main', ['$scope', function Main($scope) {
 		$scope.game = new MultipleChoiceGame()
 		$scope.game.init(lvl)
 	}
+	this.startMemory = function(lvl) {
+		this.isactive = 'memory'
+		$scope.game = new Memory()
+		$scope.game.init(lvl)
+	}
 	
 	
 	this.selectAnswer = function(choice) {
@@ -106,6 +111,40 @@ var ListWords = function() {
 	}
 }
 
+var Memory = function() {
+	this.init = function(lvlName) {
+		this.turnedCards = []
+		this.cards = new Array(16)
+		var indexSel = getRandomIndex(data[lvlName]['mg'].length, 8)
+		for (var i=0; i < indexSel.length; i++) {
+			this.cards[i*2] = {index:indexSel[i], value:data[lvlName]['mg'][indexSel[i]], lang:'mg',class:'hidden'}
+			this.cards[i*2 + 1] = {index:indexSel[i], value:data[lvlName]['de'][indexSel[i]],lang:'de',class:'hidden'}
+		}
+		shuffle(this.cards)
+	}
+	this.turn = function(index) {
+		if (this.turnedCards.length > 1) {
+			this.cards.forEach(function(card) {
+				if (card.class=='shown') {
+					card.class='hidden'
+				}
+			})
+			this.turnedCards = [];
+		}
+		this.turnedCards.push(index);
+		this.cards[index].class="shown";
+		if (this.turnedCards.length == 2) {
+			var c1 = this.cards[this.turnedCards[0]]
+			var c2 = this.cards[this.turnedCards[1]]
+			
+			if (c1.index===c2.index) {
+				c1.class='done';
+				c2.class='done';
+			}
+		}
+	}
+}
+
 
 function getRandomIndex(len, n) {
     var result = new Array(n),
@@ -121,4 +160,19 @@ function getRandomIndex(len, n) {
         choices.splice(x, 1);
     }
     return result;
+}
+
+/**
+http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript
+ * Shuffles array in place.
+ * @param {Array} a items The array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
 }
